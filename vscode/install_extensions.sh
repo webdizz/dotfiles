ca091701782a0afcc062027799d952145cb05b41
Izzet Mustafaiev <izzet@mustafaiev.com>
2017-10-17 15:19:34 +0300

    Add posibility to install VSCode extensions

---
+++ vscode/install_extensions.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Install all VSCode extensions from given string
+
+for ext in 'christian-kohler.path-intellisense' \
+            'PeterJausovec.vscode-docker' \
+            'codezombiech.gitignore' \
+            'donjayamanne.python' \
+            'HookyQR.beautify' \
+            'lukehoban.go' \
+            'redhat.java' \
+            'EditorConfig.editorconfig' \
+            'formulahendry.auto-close-tag' \
+            'DotJoshJohnson.xml' \
+            'humao.rest-client' \
+            'streetsidesoftware.code-spell-checker' \
+            'rebornix.ruby' \
+            'k--kato.intellij-idea-keybindings' \
+            'gerane.theme-solarized-light' \
+            'gerane.theme-solarized-dark' \
+            'formulahendry.terminal'; do
+    code  --install-extension "$ext"
+done
