e757bf9e42493afe3bb24d452278a28e316597de
Izzet Mustafaiev <webdizz@gmail.com>
2018-01-15 18:16:46 +0200

    Update default versions of Java ecosystem + Kotlin

--- install_java_eco.sh
+++ install_java_eco.sh
@@ -3,7 +3,10 @@
 
 source ~/.sdkman/bin/sdkman-init.sh
 
-for ext in 'java 8u144-oracle' \
-            'gradle 4.2.1'; do
+for ext in 'java 8u151-oracle' \
+                'java 9.0.1-oracle' \
+                'gradle 4.4.1' \
+                'kotlin 1.2.10' \
+                ; do
     sdk install $ext
 done
