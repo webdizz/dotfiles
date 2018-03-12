a74b1150f08b0fb0e11c84a6994d6166d1158525
Izzet Mustafaiev <webdizz@gmail.com>
2018-03-12 17:24:50 +0200

    Enhance post_setup.sh to handle MacOS and Linux based installations

--- script/post_setup.sh
+++ script/post_setup.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 # set up additional software
 
-$HOME/.dotfiles/install_java_eco.sh
-$HOME/.vscode/install_extensions.sh
+$HOME/.dotfiles/script/install_java_eco.sh
+
+if [ "$(uname 2> /dev/null)" != "Linux" ]; then
+    # MacOS related 
+	$HOME/.vscode/install_extensions.sh
+else
+	# Nothing here for Linux for now
+fi
+
