024de278113afcfb6475453ec2aed39ce85fc3fd
Izzet Mustafaiev <webdizz@gmail.com>
2018-03-23 12:12:01 +0200

    Add openssl certificate generation

---
+++ script/gen-cert.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Generates SSL certificates
+
+if [ "$1" != "" ]; then
+    cert_name=$1
+else
+    cert_name='some.host'
+fi
+
+if [ "$2" != "" ]; then
+    cert_base_path=$2
+else
+    cert_base_path='/tmp'
+fi
+
+echo "Cert will be installed into $cert_base_path/$cert_name.[key|crt]"
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout "$cert_base_path/$cert_name.key" -out "$cert_base_path/$cert_name.crt" \
+    -subj "/C=UA/ST=Kyiv/L=Kyiv/O=$cert_name/CN=$cert_name"
+
+#openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
