eb33d922b1e2b952da89c569e162562b43203d8a
Izzet Mustafaiev <izzet@mustafaiev.com>
2019-06-26 15:16:31 +0300

    Fix Java eco versions

--- script/install_java_eco.sh
+++ script/install_java_eco.sh
@@ -3,10 +3,9 @@
 
 . ~/.sdkman/bin/sdkman-init.sh
 
-for ext in 'java 8u161-oracle' \
-           'java 9.0.4-oracle' \
-           'gradle 4.6' \
-           'kotlin 1.2.30' \
+for ext in 'java 8.0.212-amzn' \
+           'gradle 5.5-rc-4' \
+           'kotlin 1.3.40' \
            ; do
     sdk install $ext
 done
