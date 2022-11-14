e838f7e56909cf00e6583378b3467f642a0b590d
Izzet Mustafaiev <izzet@mustafaiev.com>
2022-11-14 11:54:00 -0800

    Switch to use default stable versions for Java tools

--- script/install_java_eco.sh
+++ script/install_java_eco.sh
@@ -3,9 +3,9 @@
 
 . ~/.sdkman/bin/sdkman-init.sh
 
-for ext in 'java 8.0.212-amzn' \
-           'gradle 5.5-rc-4' \
-           'kotlin 1.3.40' \
+for ext in 'java' \
+           'gradle' \
+           'kotlin' \
            ; do
     sdk install $ext
 done
